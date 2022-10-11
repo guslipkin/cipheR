adbb9bf638ad9f222002e1732c299ae849ee5133
guslipkin <gus.lipkin@gmail.com>
2022-10-10 20:10:24 -0400

    Just need to test for x

---
+++ R/cipher.R
@@ -0,0 +1,96 @@
+#' Encrypt or decrypt a Caesar Cipher
+#'
+#' @description This can be used to create (encrypt) and solve (decrypt) a
+#'   Caesar cipher. The function does not differentiate between the two.
+#'
+#' @param x A vector to be shifted
+#' @param n (Default: 1) The number of places to shift by. This can be either
+#'   positive or negative. Zero returns x as it was given to the function.
+#' @param preserve_spaces (Default: TRUE) A boolean describing if spaces should
+#'   be preserved. This is helpful when working with sentences.
+#' @param dict The dictionary used for shifting. This defaults to NULL in which
+#'   case a dictionary is built from the sorted unique values of x.
+#' @param preset A pre-made dictionary using ASCII codes from
+#'   \url{://www.ascii-code.com/}. Note that `delete` is excluded as a
+#'   character.
+#'   * `NULL` (the default)
+#'   * `"alphanumeric"`: ASCII characters 48:57, 65:90, and 97:122. Numbers 0-9
+#'   and both uppercase and lowercase letters from the English alphabet.
+#'   * `"keyboard"`: ASCII characters 32:126. The characters you'll find on a
+#'   US English keyboard.
+#'   * `"letters"`: ASCII characters 65:90 and 97:122. Both uppercase and
+#'   lowercase letters from the English alphabet.
+#'   * `"lowercase"`: ASCII characters 97:122. Lowercase letters from the
+#'   English alphabet.
+#'   * `"uppercase"`: ASCII characters 65:90. Uppercase letters from the
+#'   English alphabet.
+#'
+#' @returns A character vector of length one that has been shifted.
+caesar <- function(x, n = 1, preserve_spaces = TRUE, dict = NULL, preset = NULL) {
+
+  if (!is.null(dict) & !is.null(preset)) {
+    warning("Both a dict and a preset was provided, only the dict will be used.")
+  }
+
+  if (n == 0) {
+    warning("Shifting by zero doesn't do anything...")
+    return(x)
+  }
+
+  if (!is.character(x)) {
+    x <- as.character(x)
+  }
+
+  if (length(x) == 1) {
+    x <- unlist(strsplit(x, ""))
+  }
+
+  if (is.null(dict) & is.null(preset)) {
+    dict <- sort(unique(x))
+  } else if (!is.null(preset)) {
+    if (preset == "alphanumeric") {
+      dict <- rawToChar(as.raw(c(48:57, 65:90, 97:122)))
+    } else if (preset == "keyboard") {
+      dict <- rawToChar(as.raw(32:126))
+    } else if (preset == "letters") {
+      dict <- rawToChar(as.raw(c(65:90, 97:122)))
+    } else if (preset == "lowercase") {
+      dict <- rawToChar(as.raw(c(97:122)))
+    } else if (preset == "uppercase") {
+      dict <- rawToChar(as.raw(c(65:90)))
+    } else {
+      stop("It looks like you may have a typo in your presets. You can double-check the presets by running ?caesar in your console.")
+    }
+  }
+
+  if (length(dict) == 1) {
+    dict <- unlist(strsplit(dict, ""))
+  }
+
+  if (!all(x %in% dict)) {
+    stop("Not all values of x are in the character set. Please choose a different character set.")
+  }
+
+  if (preserve_spaces) {
+    isSpace <- which(dict == " ")
+    if (length(isSpace > 0)) {
+      dict <- dict[-which(dict == " ")]
+    }
+  }
+
+  x <-
+    sapply(x, function(y) {
+      if (preserve_spaces & y == " ") {
+        return(" ")
+      }
+      hop <- which(y == dict) + n
+      hop <- hop %% length(dict)
+      if (hop == 0) {
+        hop <- length(dict)
+      }
+      return(dict[hop])
+    })
+  x <- paste0(x, collapse = "")
+
+  return(x)
+}
---
+++ tests/testthat/test-caesar.R
@@ -0,0 +1,48 @@
+test_that("Shift 0 warns and works", {
+  expect_warning(caesar("abcde", 0))
+  expect_equal(caesar("abcde", 0), "abcde")
+})
+test_that("Shift 1 works", {
+  expect_equal(caesar("abcde", 1), "bcdea")
+})
+test_that("Shift -1 works", {
+  expect_equal(caesar("abcde", -1), "eabcd")
+})
+
+test_that("Spaces are preserved", {
+  expect_equal(caesar("a b c", 1), "b c a")
+})
+test_that("Spaces are not preserved", {
+  expect_equal(caesar("a b c", 1, preserve_spaces = FALSE), "baca ")
+})
+
+test_that("When no dictionary is provided, x is used", {
+  expect_equal(caesar("abcde", 1), "bcdea")
+})
+
+test_that("Cannot provide both a dict and a preset", {
+  expect_warning(caesar("abcde", 1, dict = "abcde", preset = "letters"))
+})
+
+test_that("The alphanumeric preset works", {
+  expect_equal(caesar("abc123", 1, preset = "alphanumeric"), "bcd234")
+})
+test_that("The keyboard preset works", {
+  expect_equal(caesar("abc123!@#", 1, preset = "keyboard"), "bcd234\"A$")
+})
+test_that("The letters preset works", {
+  expect_equal(caesar("abcABC", 1, preset = "letters"), "bcdBCD")
+})
+test_that("The lowercase preset works", {
+  expect_equal(caesar("abcde", 1, preset = "lowercase"), "bcdef")
+})
+test_that("The uppercase preset works", {
+  expect_equal(caesar("ABCDE", 1, preset = "uppercase"), "BCDEF")
+})
+test_that("A typo in the preset gives an error", {
+  expect_error(caesar("abc", 1, preset = "abc"))
+})
+
+test_that("dict must contain all values in x", {
+  expect_error(caesar("abc", 1, dict = "def"))
+})
